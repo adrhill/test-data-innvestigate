d7f72bdf78e7f4c3252409623508f453ca80db70
Adrian Hill <adrian.hill@mailbox.org>
2021-03-10 17:02:49 +0100

    Collect tensors during reverse pass

--- src/test_data_innvestigate/generate/vgg16.py
+++ src/test_data_innvestigate/generate/vgg16.py
@@ -18,8 +18,6 @@ print(base_dir)
 
 def run(image, show_plot=False):
     """Load an image and run it through VGG16, applying LRP."""
-
-    # Code snippet.
     fig, ax = show_image(image / 255)
     if show_plot is True:
         plt.show()
@@ -30,12 +28,25 @@ def run(image, show_plot=False):
     model = innvestigate.utils.model_wo_softmax(model)
 
     # Create analyzer
-    analyzer = innvestigate.create_analyzer("deep_taylor", model)
+    analyzer = innvestigate.analyzer.DeepTaylor(model, reverse_keep_tensors=True)
 
     # Add batch axis and preprocess
     x = preprocess(image[None])
     # Apply analyzer w.r.t. maximum activated output-neuron
     a = analyzer.analyze(x)
+
+    # print(analyzer.state)
+    reversed_tensors = analyzer._reversed_tensors
+
+    # unzip reverse tensors to strip indices
+    tensor_indices, reversed_tensors = zip(*reversed_tensors)
+
+    for tensor in reversed_tensors:
+        print(tensor)
+
+    print(tensor_indices)
+
+    print(reversed_tensors[-1] == a)
 
     # Aggregate along color channels and normalize to [-1, 1]
     a = a.sum(axis=np.argmax(np.asarray(a.shape) == 3))
