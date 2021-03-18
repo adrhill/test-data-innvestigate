c479bdcd3fcaa540d10c8cc29761ba2b8867c683
Adrian Hill <adrian.hill@mailbox.org>
2021-03-18 20:50:46 +0100

    Analyze VGG16 with all LRP analyzers

--- src/test_data_innvestigate/generate/vgg16.py
+++ src/test_data_innvestigate/generate/vgg16.py
@@ -1,6 +1,7 @@
 import os
 
 import innvestigate
+import innvestigate.analyzer.relevance_based.relevance_analyzer as ranalyzer
 
 import h5py
 
@@ -13,10 +14,31 @@ import numpy as np
 from ..utils.images import load_image, show_heatmap, show_image
 
 ROOT_DIR = os.path.abspath(os.curdir)
+LRP_ANALYZERS = [
+    "LRPZ",
+    "LRPZIgnoreBias",
+    "LRPEpsilon",
+    "LRPEpsilonIgnoreBias",
+    "LRPWSquare",
+    "LRPFlat",
+    "LRPAlpha2Beta1",
+    "LRPAlpha2Beta1IgnoreBias",
+    "LRPAlpha1Beta0",
+    "LRPAlpha1Beta0IgnoreBias",
+    "LRPZPlus",
+    "LRPZPlusFast",
+    "LRPSequentialPresetA",
+    "LRPSequentialPresetB",
+    "LRPSequentialPresetAFlat",
+    "LRPSequentialPresetBFlat",
+    "LRPSequentialPresetBFlatUntilIdx",
+]
 
 
 def generate():
     """Load an image and run it through VGG16, applying LRP."""
+    print("Generating data on VGG16...")
+
     # Load image
     image = load_image(
         os.path.join(
@@ -35,30 +57,40 @@ def generate():
     model = innvestigate.utils.model_wo_softmax(model)
 
     # Create analyzer
-    analyzer = innvestigate.analyzer.DeepTaylor(model, reverse_keep_tensors=True)
 
     # Add batch axis and preprocess
     x = preprocess(image[None])
-    
-    # Apply analyzer w.r.t. maximum activated output-neuron
-    a = analyzer.analyze(x)
 
-    # Obtain layerwise tensors, stripping first
-    relevances = analyzer._reversed_tensors#[1:]
-    # unzip reverse tensors to strip indices
-    indices, relevances = zip(*relevances)
-
-    assert np.allclose(
-        relevances[1], a
-    ), "_reversed_tensors output differs from final attribution"
-
-    # Save data
+    # Write to hdf5 file
     data_path = os.path.join(ROOT_DIR, "data", "models", "vgg16.hdf5")
-
     with h5py.File(data_path, "w") as f:
-        f.create_dataset("input_data", data=x)
-        f.attrs['model_name'] = "vgg16"
+        f.create_dataset("input", data=x)
+        f.attrs["model_name"] = "vgg16"
 
-        rs = f.create_group("layerwise_relevances")
-        for idx, rel in zip(indices, relevances):
-            rs.create_dataset(str(idx[0]), data=rel)
+        rels = f.create_group("layerwise_relevances")
+
+        # Run all analyzers
+        for analyzer_name in LRP_ANALYZERS:
+            # Get analyzer class
+            Analyzer = getattr(ranalyzer, analyzer_name)
+
+            # Construct analyzer
+            analyzer = Analyzer(model, reverse_keep_tensors=True)
+            print("\t... using {}: {}".format(analyzer_name, analyzer))
+
+            # Apply analyzer w.r.t. maximum activated output-neuron
+            a = analyzer.analyze(x)
+
+            # Obtain layerwise tensors
+            relevances = analyzer._reversed_tensors
+            # unzip reverse tensors to strip indices
+            indices, relevances = zip(*relevances)
+
+            assert np.allclose(
+                relevances[1], a
+            ), "_reversed_tensors output differs from final attribution"
+
+            # Save relevances for this analyzer
+            ana = rels.create_group(analyzer_name)
+            for idx, rel in zip(indices, relevances):
+                ana.create_dataset(str(idx[0]), data=rel)
