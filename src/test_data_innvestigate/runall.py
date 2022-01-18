9c36251d7596198b1a36442fab36be5bc0265695
Adrian Hill <adrian.hill@mailbox.org>
2022-01-18 18:22:55 +0100

    Drop VGG tests

--- src/test_data_innvestigate/runall.py
+++ src/test_data_innvestigate/runall.py
@@ -1,10 +1,8 @@
 """Run all scripts that generate test data."""
 from .generate import single_layer
-from .generate import vgg16
 
 
 def main():
     """Generate all test data."""
 
     single_layer.generate()
-    vgg16.generate()
