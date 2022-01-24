753ba8b534d435c1b07a3b6eb4a3657133318837
Adrian Hill <adrian.hill@mailbox.org>
2022-01-24 18:50:05 +0100

    Make dataset deterministic, remove `SmoothGrad`

--- src/test_data_innvestigate/generate/single_layer.py
+++ src/test_data_innvestigate/generate/single_layer.py
@@ -8,9 +8,11 @@ from pathlib import Path
 import h5py
 import keras
 import numpy as np
+import tensorflow as tf
 
 from test_data_innvestigate.utils.analyzers import METHODS
 
+np.random.seed(123)
 ROOT_DIR = os.path.abspath(os.curdir)
 
 # Create input data representative of a single 2D image with 3 channels
@@ -23,31 +25,14 @@ BATCH_SIZE = 1
 KERNEL_SIZE = (3, 3)
 POOL_SIZE = (2, 2)
 
-LAYERS_2D = {
-    "Dense": keras.layers.Dense(5, input_shape=INPUT_SHAPE),
-    "Dense_relu": keras.layers.Dense(5, activation="relu", input_shape=INPUT_SHAPE),
-    "Conv2D": keras.layers.convolutional.Conv2D(
-        5, KERNEL_SIZE, input_shape=INPUT_SHAPE
-    ),
-    "Conv2D_relu": keras.layers.convolutional.Conv2D(
-        5, KERNEL_SIZE, activation="relu", input_shape=INPUT_SHAPE
-    ),
-    "AveragePooling2D": keras.layers.pooling.AveragePooling2D(
-        POOL_SIZE, input_shape=INPUT_SHAPE
-    ),
-    "MaxPooling2D": keras.layers.pooling.MaxPooling2D(
-        POOL_SIZE, input_shape=INPUT_SHAPE
-    ),
-}
 
-
-def get_single_layer_model(layer):
+def get_single_layer_model(layer, layer_name: str):
     """Return Keras model consisting of `layer`."""
-    weights = layer.get_weights()
-    model = keras.Sequential()
-    model.add(layer)
-    model.add(keras.layers.Flatten())
-
+    inputs = keras.layers.Input(shape=INPUT_SHAPE)
+    activations = layer(inputs)
+    outputs = keras.layers.Flatten()(activations)
+    model = keras.Model(inputs=inputs, outputs=outputs, name=layer_name)
+    weights = model.get_weights()
     return model, weights
 
 
@@ -61,18 +46,43 @@ def generate():
 
     for analyzer_name, m in METHODS.items():
         print("\t... using {}".format(analyzer_name))
+
         method, kwargs = m
 
         # Write to hdf5 file
         data_path = os.path.join(ROOT_DIR, "data", "layer", analyzer_name + ".hdf5")
         with h5py.File(data_path, "w") as f:
 
+            keras.backend.clear_session()
+            session = tf.Session(graph=tf.get_default_graph())
+            keras.backend.set_session(session)
+            tf.set_random_seed(123)
+
+            layers_2d = {
+                "Dense": keras.layers.Dense(5, input_shape=INPUT_SHAPE),
+                "Dense_relu": keras.layers.Dense(
+                    5, activation="relu", input_shape=INPUT_SHAPE
+                ),
+                "Conv2D": keras.layers.convolutional.Conv2D(
+                    5, KERNEL_SIZE, input_shape=INPUT_SHAPE
+                ),
+                "Conv2D_relu": keras.layers.convolutional.Conv2D(
+                    5, KERNEL_SIZE, activation="relu", input_shape=INPUT_SHAPE
+                ),
+                "AveragePooling2D": keras.layers.pooling.AveragePooling2D(
+                    POOL_SIZE, input_shape=INPUT_SHAPE
+                ),
+                "MaxPooling2D": keras.layers.pooling.MaxPooling2D(
+                    POOL_SIZE, input_shape=INPUT_SHAPE
+                ),
+            }
+
             f.create_dataset("input", data=x)
             f.attrs["analyzer_name"] = analyzer_name
 
-            for layer_name, layer in LAYERS_2D.items():
+            for layer_name, layer in layers_2d.items():
                 # Get model
-                model, weights = get_single_layer_model(layer)
+                model, weights = get_single_layer_model(layer, layer_name)
                 # Analyze model
                 analyzer = method(model, **kwargs)
                 a = analyzer.analyze(x)
--- src/test_data_innvestigate/utils/analyzers.py
+++ src/test_data_innvestigate/utils/analyzers.py
@@ -2,63 +2,41 @@
 Lists and helper functions to iterate over all iNNvestigate analyzers.
 """
 
-from innvestigate.analyzer.deeptaylor import BoundedDeepTaylor
-from innvestigate.analyzer.deeptaylor import DeepTaylor
-from innvestigate.analyzer.gradient_based import BaselineGradient
-from innvestigate.analyzer.gradient_based import Deconvnet
-from innvestigate.analyzer.gradient_based import Gradient
-from innvestigate.analyzer.gradient_based import GuidedBackprop
-from innvestigate.analyzer.gradient_based import InputTimesGradient
-from innvestigate.analyzer.gradient_based import IntegratedGradients
-from innvestigate.analyzer.gradient_based import SmoothGrad
-from innvestigate.analyzer.relevance_based.relevance_analyzer import BaselineLRPZ
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRP
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPAlpha1Beta0
+from innvestigate.analyzer.deeptaylor import BoundedDeepTaylor, DeepTaylor
+from innvestigate.analyzer.gradient_based import (
+    Deconvnet,
+    Gradient,
+    GuidedBackprop,
+    InputTimesGradient,
+    IntegratedGradients,
+    SmoothGrad,
+)
 from innvestigate.analyzer.relevance_based.relevance_analyzer import (
+    LRPZ,
+    LRPAlpha1Beta0,
     LRPAlpha1Beta0IgnoreBias,
-)
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPAlpha2Beta1
-from innvestigate.analyzer.relevance_based.relevance_analyzer import (
+    LRPAlpha2Beta1,
     LRPAlpha2Beta1IgnoreBias,
-)
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPAlphaBeta
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPEpsilon
-from innvestigate.analyzer.relevance_based.relevance_analyzer import (
+    LRPEpsilon,
     LRPEpsilonIgnoreBias,
-)
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPFlat
-from innvestigate.analyzer.relevance_based.relevance_analyzer import (
+    LRPFlat,
     LRPSequentialPresetA,
-)
-from innvestigate.analyzer.relevance_based.relevance_analyzer import (
     LRPSequentialPresetAFlat,
-)
-from innvestigate.analyzer.relevance_based.relevance_analyzer import (
     LRPSequentialPresetB,
-)
-from innvestigate.analyzer.relevance_based.relevance_analyzer import (
     LRPSequentialPresetBFlat,
+    LRPWSquare,
+    LRPZIgnoreBias,
+    LRPZPlus,
+    LRPZPlusFast,
 )
-from innvestigate.analyzer.relevance_based.relevance_analyzer import (
-    LRPSequentialPresetBFlatUntilIdx,
-)
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPWSquare
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPZ
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPZIgnoreBias
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPZPlus
-from innvestigate.analyzer.relevance_based.relevance_analyzer import LRPZPlusFast
-from innvestigate.analyzer.wrapper import AugmentReduceBase
-from innvestigate.analyzer.wrapper import GaussianSmoother
-from innvestigate.analyzer.wrapper import PathIntegrator
 
 METHODS = {
     # Gradient based
-    "Gradient": (Gradient, {}),
     "InputTimesGradient": (InputTimesGradient, {}),
+    "Gradient": (Gradient, {}),
     "Deconvnet": (Deconvnet, {}),
     "GuidedBackprop": (GuidedBackprop, {}),
     "IntegratedGradients": (IntegratedGradients, {}),
-    "SmoothGrad": (SmoothGrad, {}),
     # Relevance based
     "LRPZ": (LRPZ, {}),
     "LRPZ_Flat_input_layer_rule": (LRPZ, {"input_layer_rule": "Flat"}),
@@ -66,7 +44,6 @@ METHODS = {
     "LRPZIgnoreBias": (LRPZIgnoreBias, {}),
     "LRPZPlus": (LRPZPlus, {}),
     "LRPZPlusFast": (LRPZPlusFast, {}),
-    "BaselineLRPZ": (BaselineLRPZ, {}),
     "LRPAlpha1Beta0": (LRPAlpha1Beta0, {}),
     "LRPAlpha1Beta0IgnoreBias": (LRPAlpha1Beta0IgnoreBias, {}),
     "LRPAlpha2Beta1": (LRPAlpha2Beta1, {}),
